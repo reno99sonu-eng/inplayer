a2e6938dda4c035e0dfa6ae5d010d26c4ef348a1
Snigdharup Sen <reno99sonu@gmail.com>
2026-08-27 17:15:33 +0530

    chore(android): update package applicationId to com.inplayer.app

--- INPLAYER_Android/android/app/build.gradle.kts
+++ INPLAYER_Android/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.inplayer_android"
+    namespace = "com.inplayer.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -15,10 +15,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.inplayer_android"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.inplayer.app"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
